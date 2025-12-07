50cadadd21278b1b64f3d7c4d061bce9cddeba4f
C Lahiri <helloclahiri@outlook.com>
2025-12-07 22:04:24 +0530

    Mark1 Backend code with sample data for geospatial location

---
+++ walksafeai-backend/src/main/java/com/v1/walksafeai_backend/WalksafeaiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.v1.walksafeai_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalksafeaiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalksafeaiBackendApplication.class, args);
+	}
+
+}
---
+++ walksafeai-backend/src/test/java/com/v1/walksafeai_backend/WalksafeaiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.v1.walksafeai_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalksafeaiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
